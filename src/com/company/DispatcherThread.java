e3a8f1f369be9dc210f8e3874724b94befbcdb5d
C00289039-ULL <116237424+C00289039-ULL@users.noreply.github.com>
2022-11-21 20:47:01 -0600

    updated code

--- src/com/company/DispatcherThread.java
+++ src/com/company/DispatcherThread.java
@@ -23,80 +23,78 @@ public class DispatcherThread extends Thread{
         this.id = i;
     }
     
+    //Code changes by Alexandre' Davis
     public void run(){
-        while(TaskList.size()!=0){
-            switch(method) {
-                case 1://FCFS logic here
-                    break;
-                case 2://RR logic here
-                    break;
-                case 3://N-SJF logic here
-                    break;
-                case 4:
-                    Scanner sc = new Scanner(System.in);
-                    System.out.println ("Processes #: " );
-                    int n = sc.nextInt();
-                    int processID[] = new int[n];
-                    int at[] = new int[n]; 
-                    int bt[] = new int[n]; 
-                    int ct[] = new int[n];
-                    int ta[] = new int[n];
-                    int wt[] = new int[n];
-                    int f[] = new int[n]; 
-                    int k[]= new int[n];
-                    int i, st=0, tot=0;
-                    float avgwt = 0, avgta = 0;
+        switch(method) {
+            case 1://FCFS logic here
+                break;
+            case 2://RR logic here
+                break;
+            case 3://N-SJF logic here
+                break;
+            case 4:
+                Scanner sc = new Scanner(System.in);
+                System.out.println ("Processes #: " );
+                int n = sc.nextInt();
+                int processID[] = new int[n];
+                int at[] = new int[n]; 
+                int bt[] = new int[n]; 
+                int ct[] = new int[n];
+                int ta[] = new int[n];
+                int wt[] = new int[n];
+                int f[] = new int[n]; 
+                int k[]= new int[n];
+                int i, st=0, tot=0;
+                float avgwt = 0, avgta = 0;
                     
+                for (i = 0; i < n; i++) {
+                    processID[i] = i + 1;
+                    System.out.println("enter process " + (i + 1) + " arrival time:");
+                    at[i] = sc.nextInt();
+                    System.out.println("enter process " + (i + 1) + " brust time:");
+                    bt[i] = sc.nextInt();
+                    k[i] = bt[i];
+                    f[i] = 0;
+                }
+                boolean a = true;
+                while (true) {
+                    int c = n, min = 999;
+                    if (tot == n)
+                        break;
                     for (i = 0; i < n; i++) {
-                        processID[i] = i + 1;
-                        System.out.println("enter process " + (i + 1) + " arrival time:");
-                        at[i] = sc.nextInt();
-                        System.out.println("enter process " + (i + 1) + " brust time:");
-                        bt[i] = sc.nextInt();
-                        k[i] = bt[i];
-                        f[i] = 0;
-                    }
-                    boolean a = true;
-                    while (true) {
-                        int c = n, min = 999;
-                        if (tot == n)
-                            break;
-                        for (i = 0; i < n; i++) {
-                            if ((at[i] <= st) && (f[i] == 0) && (bt[i] < min)) {
-                                min = bt[i];
-                                c = i;
-                            }
-                        }
-                        if (c == n) {
-                            st++;
-                        } else {
-                            bt[c]--;
-                            st++;
-                            if (bt[c] == 0) {
-                                ct[c] = st;
-                                f[c] = 1;
-                                tot++;
-                            }
+                        if ((at[i] <= st) && (f[i] == 0) && (bt[i] < min)) {
+                            min = bt[i];
+                            c = i;
                         }
                     }
-                    for (i = 0; i < n; i++) {
-                        ta[i] = ct[i] - at[i];
-                        wt[i] = ta[i] - k[i];
-                        avgwt += wt[i];
-                        avgta += ta[i];
+                    if (c == n) {
+                        st++;
+                    } else {
+                        bt[c]--;
+                        st++;
+                        if (bt[c] == 0) {
+                            ct[c] = st;
+                            f[c] = 1;
+                            tot++;
+                        }
                     }
-                    System.out.println("processID  arrival  burst  complete  turn  waiting");
-                    for (i = 0; i < n; i++) {
-                        System.out.println(
-                                processID[i] + "\t" + at[i] + "\t" + k[i] + "\t" + ct[i] + "\t" + ta[i] + "\t" + wt[i]);
-                    }
-                    System.out.println ("\naverage tat is "+ (float)(avgta/n));
-                    System.out.println ("average wt is "+ (float)(avgwt/n));
-                    sc.close();
-                    break;
-                default:
-                    break;
-            }
+                }
+                for (i = 0; i < n; i++) {
+                    ta[i] = ct[i] - at[i];
+                    wt[i] = ta[i] - k[i];
+                    avgwt += wt[i];
+                    avgta += ta[i];
+                }
+                System.out.println("processID  arrival  burst  complete  turn  waiting");
+                for (i = 0; i < n; i++) {
+                    System.out.println(processID[i] + "\t" + at[i] + "\t" + k[i] + "\t" + ct[i] + "\t" + ta[i] + "\t" + wt[i]);
+                }
+                System.out.println ("\naverage tat is "+ (float)(avgta/n));
+                System.out.println ("average wt is "+ (float)(avgwt/n));
+                sc.close();
+                break;
+            default:
+                break;
         }
     }
 }
